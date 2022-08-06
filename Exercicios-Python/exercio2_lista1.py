3fb5f3a6211957d6bad312e7ea4b9abe1bed2f35
Perkles <leticiamorei@gmail.com>
2022-08-06 16:17:19 -0300

    Inserçao de exercicios python

---
+++ Exercicios-Python/exercio2_lista1.py
@@ -0,0 +1,10 @@
+import string
+from typing import Tuple
+fim = str
+pi = 3.14159
+
+raio = float(input())
+
+area = pi * (raio*raio)
+
+print("A=%0.4f"%area)
---
+++ Exercicios-Python/exercio3_lista1.py
@@ -0,0 +1,7 @@
+A = float(input())
+B = float(input())
+C = float(input())
+
+media = ((A*2) + (B*3) + (C*5))
+
+print("MEDIA =%1.1f" %media)
